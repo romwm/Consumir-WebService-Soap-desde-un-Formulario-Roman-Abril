8239c52fa453c42a44b551444154e36e1d5c9017
romwm <73566034+romwm@users.noreply.github.com>
2023-11-13 13:19:46 -0300

    Add files via upload

---
+++ Consumo Form Roman Abril/ConsumoFormRomanAbril/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace ConsumoFormRomanAbril
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dia = new System.Windows.Forms.Label();
+            this.cotdia = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(190, 186);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(231, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Fecha de hoy: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Modern No. 20", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(127, 266);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(294, 36);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Cotización del día:";
+            // 
+            // dia
+            // 
+            this.dia.AutoSize = true;
+            this.dia.Font = new System.Drawing.Font("Modern No. 20", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dia.Location = new System.Drawing.Point(427, 186);
+            this.dia.Name = "dia";
+            this.dia.Size = new System.Drawing.Size(231, 36);
+            this.dia.TabIndex = 2;
+            this.dia.Text = "Fecha de hoy: ";
+            
+            // 
+            // cotdia
+            // 
+            this.cotdia.AutoSize = true;
+            this.cotdia.Font = new System.Drawing.Font("Modern No. 20", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cotdia.Location = new System.Drawing.Point(427, 266);
+            this.cotdia.Name = "cotdia";
+            this.cotdia.Size = new System.Drawing.Size(231, 36);
+            this.cotdia.TabIndex = 3;
+            this.cotdia.Text = "Fecha de hoy: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cotdia);
+            this.Controls.Add(this.dia);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label dia;
+        private System.Windows.Forms.Label cotdia;
+    }
+}
+
